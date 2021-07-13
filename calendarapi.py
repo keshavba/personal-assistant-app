25a2677ae65d3d894f14bebe0b7c07e805962628
Keshav Balaji <keshav.balaji@mavs.uta.edu>
2021-07-12 21:07:14 -0500

    Changes to CalendarAPI and gitignore

--- calendarapi.py
+++ calendarapi.py
@@ -7,7 +7,7 @@ if path.exists('token.pkl') == False:
     scopes = ['https://www.googleapis.com/auth/calendar']
     flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', scopes=scopes)
     credentials = flow.run_console()
-    
+    pickle.dump(credentials, open('token.pkl', 'wb'))
 
 credentials = pickle.load(open('token.pkl', 'rb'))
 service = build('calendar', 'v3', credentials=credentials)
